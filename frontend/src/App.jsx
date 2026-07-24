a5b29c738212e6b0a78aa7a5a43b6c5879ab50f1
abhi-afk-dev <abhilash000006@gmail.com>
2026-07-24 18:53:07 +0530

    updated direct transitioning to main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,17 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      if (session) {
+        window.location.href = "https://app.takumi.run/mvp";
+      }
+    };
+    checkUser();
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
