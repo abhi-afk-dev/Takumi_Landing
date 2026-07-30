55c128e172222c47b15c282374b7b842f6e972db
abhi-afk-dev <abhilash000006@gmail.com>
2026-07-30 06:10:37 +0530

    removed the redirecting to main app3

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom";
 import { track } from "@vercel/analytics";
 
 function Button() {
-  // track("Navigated_To_App");
-  // window.location.href = "https://app.takumi.run";
+  // Fire the analytics event only when clicked
+  const handleTrackClick = () => {
+    track("Navigated_To_App");
+  };
+
   return (
     <a
       href="https://app.takumi.run/login"
       target="_blank"
       rel="noopener noreferrer"
+      onClick={handleTrackClick}
     >
       <button className="px-4 py-2 md:px-8 md:py-3 jet bg-[#F59E08] text-black hover:bg-[#F4F4F5] hover:text-black text-sm md:text-lg">
         Join Alpha
