67b0eabc9dd8234d4a13fa503f182a3c0c18de1d
abhi-afk-dev <abhilash000006@gmail.com>
2026-07-30 05:59:31 +0530

    added the track metrics

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import { track } from "@vercel/analytics";
+
 function Button() {
+  track("Navigated_To_App");
+  window.location.href = "https://app.takumi.run";
   return (
-    <a href="https://app.takumi.run/login" target="_blank" rel="noopener noreferrer">
+    <a
+      href="https://app.takumi.run/login"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <button className="px-4 py-2 md:px-8 md:py-3 jet bg-[#F59E08] text-black hover:bg-[#F4F4F5] hover:text-black text-sm md:text-lg">
         Join Alpha
       </button>
-
     </a>
   );
 }
